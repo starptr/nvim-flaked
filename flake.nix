069ecf8d913d70e14e76ee0f080db92ee5fbed8e
Yuto Nishida <yuto@berkeley.edu>
2023-07-04 03:39:59 +0000

    Use unstable nixpkgs and expose hello as package dep

--- flake.nix
+++ flake.nix
@@ -1,28 +1,28 @@
 {
   inputs = {
-    nixpkgs.url = "nixpkgs/release-22.11";
+    nixpkgs.url = "nixpkgs";
     nuenv.url = "github:DeterminateSystems/nuenv";
   };
 
   outputs = { self, nixpkgs, nuenv }: let
     overlays = [ nuenv.overlays.default ];
-    systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+    systems = [
+      "x86_64-linux"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
+    ];
     forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f {
       inherit system;
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [
-          nuenv.overlays.nuenv
-        ];
-      };
+      pkgs = import nixpkgs { inherit overlays system; };
     });
   in {
     packages = forAllSystems ({ pkgs, system }: {
       default = pkgs.nuenv.mkDerivation {
         name = "hello";
+        packages = [ pkgs.hello ];
         src = ./.;
-        inherit system;
-        # This script is Nushell, not Bash
+        # This script is Nushell, not Bash!
         build = ''
           hello --greeting $"($env.MESSAGE)" | save hello.txt
           let out = $"($env.out)/share"
